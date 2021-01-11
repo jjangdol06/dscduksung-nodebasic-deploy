d25d76a8e3427102b5ded38cafd6cfa24187d396
jjangdol06 <jiyeon971023@gmail.com>
2021-01-11 13:30:45 +0900

    cors

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const { sequelize } = require('./models');
 const pageRouter = require('./routes/page');
 
 const app = express();
+app.use(cors());
 sequelize.sync();
 
 app.set('port', process.env.PORT || 3065)
